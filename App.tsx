ac50e14e1c07ee01ff3a28d25ddb6e39f08870ff
radoslav.naydenov <radoslav.naydenov@wult.io>
2023-01-16 20:17:10 +0100

    add structure

--- App.tsx
+++ App.tsx
@@ -1,61 +1,7 @@
-import * as React from "react"
-import { View, Text, Button } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import Ionicons from "@expo/vector-icons/Ionicons"
-const Tab = createBottomTabNavigator()
+import TabMenuStackNaigator from "./src/components/navigation"
 
-function TabMenuStackNaigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName
-          if (route.name === "Home") {
-            iconName = focused ? "home" : "home-outline"
-          } else if (route.name === "Search") {
-            iconName = focused ? "search" : "search-outline"
-          } else if (route.name === "Profile") {
-            iconName = focused ? "person" : "person-outline"
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName as any} size={size} color={color} />
-        }
-      })}
-    >
-      <Tab.Screen name='Home' component={HomeScreen} />
-      <Tab.Screen name='Profile' component={ProfileScreen} />
-      <Tab.Screen name='Search' component={SearchScreen} />
-    </Tab.Navigator>
-  )
-}
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
-
-function SearchScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Search Screen</Text>
-    </View>
-  )
-}
-
-function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
-    </View>
-  )
-}
-
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <TabMenuStackNaigator />
---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,34 @@
+import { Ionicons } from "@expo/vector-icons"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Home from "../../screens/Home"
+import Profile from "../../screens/Profile"
+import Search from "../../screens/Search"
+
+const Tab = createBottomTabNavigator()
+
+const TabMenuStackNaigator: React.FC = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline"
+          } else if (route.name === "Search") {
+            iconName = focused ? "search" : "search-outline"
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline"
+          }
+          return <Ionicons name={iconName as any} size={size} color={color} />
+        },
+        tabBarStyle: { padding: "5px" }
+      })}
+    >
+      <Tab.Screen name='Home' component={Home} />
+      <Tab.Screen name='Profile' component={Profile} />
+      <Tab.Screen name='Search' component={Search} />
+    </Tab.Navigator>
+  )
+}
+
+export default TabMenuStackNaigator
