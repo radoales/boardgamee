50801d8138204ce520be129f88bd73f5191a6b3d
radoslav.naydenov <radoslav.naydenov@wult.io>
2023-01-16 19:01:11 +0100

    update icon lib

--- App.tsx
+++ App.tsx
@@ -2,13 +2,31 @@ import * as React from "react"
 import { View, Text, Button } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from "@expo/vector-icons/Ionicons"
 const Tab = createBottomTabNavigator()
 
 function TabMenuStackNaigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='HomeScreen' component={HomeScreen} />
-      <Tab.Screen name='DetailsScreen' component={DetailsScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline"
+          } else if (route.name === "Search") {
+            iconName = focused ? "search" : "search-outline"
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline"
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName as any} size={size} color={color} />
+        }
+      })}
+    >
+      <Tab.Screen name='Home' component={HomeScreen} />
+      <Tab.Screen name='Profile' component={ProfileScreen} />
+      <Tab.Screen name='Search' component={SearchScreen} />
     </Tab.Navigator>
   )
 }
@@ -21,10 +39,18 @@ function HomeScreen() {
   )
 }
 
-function DetailsScreen() {
+function SearchScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
+      <Text>Search Screen</Text>
+    </View>
+  )
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Profile Screen</Text>
     </View>
   )
 }
