19dd1590eabf8db68a365bcf2cca46ae0a8c0ec3
natsgt <nataliarodas7858@gmail.com>
2023-01-16 19:36:50 +0100

    add images

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Here is where we will have the games :O</Text>
+      <Image
+        source={require("./assets/boardgamee-high-resolution-logo-color-on-transparent-background.png")}
+        style={styles.frontImage}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  frontImage: {
+    width: "25vw",
+    height: "15vh",
+    resizeMode: "contain",
   },
 });
