7cbd22fcff846b1764e1351fd7a0e8e9baff4565
natsgt <nataliarodas7858@gmail.com>
2023-01-17 19:51:24 +0100

    create input component

---
+++ src/components/PatitoInput.tsx
@@ -0,0 +1,26 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    borderWidth: 1,
+    borderRadius: 8,
+    display: "flex",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
+  input: {
+    flex: 1,
+  },
+});
+
+const PatitoInput = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <Ionicons name="search-sharp" size={24} color="black" />
+      <TextInput style={styles.input} inlineImageLeft="search_icon" />
+    </View>
+  );
+};
+
+export default PatitoInput;
--- src/screens/Search/Stack/HomeSearch.tsx
+++ src/screens/Search/Stack/HomeSearch.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "..";
+import PatitoInput from "../../../components/PatitoInput";
 
 type Props = NativeStackScreenProps<RootStackParamList, "HomeSearch">;
 
 const HomeSearch = ({ navigation }: Props) => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <PatitoInput />
       <Text onPress={() => navigation.navigate("Detail")}>Search Screen</Text>
     </View>
   );
