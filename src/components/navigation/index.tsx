0ed92dc8efa31521f47697e44154620ee9410266
natsgt <nataliarodas7858@gmail.com>
2023-01-17 19:16:31 +0100

    redirect to search stack

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,34 +1,34 @@
-import { Ionicons } from "@expo/vector-icons"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import Home from "../../screens/Home"
-import Profile from "../../screens/Profile"
-import Search from "../../screens/Search"
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../../screens/Home";
+import Profile from "../../screens/Profile";
+import SearchStack from "../../screens/Search";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const TabMenuStackNaigator: React.FC = () => {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
-          let iconName
+          let iconName;
           if (route.name === "Home") {
-            iconName = focused ? "home" : "home-outline"
+            iconName = focused ? "home" : "home-outline";
           } else if (route.name === "Search") {
-            iconName = focused ? "search" : "search-outline"
+            iconName = focused ? "search" : "search-outline";
           } else if (route.name === "Profile") {
-            iconName = focused ? "person" : "person-outline"
+            iconName = focused ? "person" : "person-outline";
           }
-          return <Ionicons name={iconName as any} size={size} color={color} />
+          return <Ionicons name={iconName as any} size={size} color={color} />;
         },
-        tabBarStyle: { padding: "5px" }
+        tabBarStyle: { padding: 5 },
       })}
     >
-      <Tab.Screen name='Home' component={Home} />
-      <Tab.Screen name='Profile' component={Profile} />
-      <Tab.Screen name='Search' component={Search} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Search" component={SearchStack} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default TabMenuStackNaigator
+export default TabMenuStackNaigator;
