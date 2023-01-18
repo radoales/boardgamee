f770cfae6efd515d2b0c5a7324d706d4d1bf7d81
natsgt <nataliarodas7858@gmail.com>
2023-01-18 18:02:31 +0100

    take icon out

--- src/screens/Search/Stack/HomeSearch.tsx
+++ src/screens/Search/Stack/HomeSearch.tsx
@@ -2,13 +2,16 @@ import { Text, View } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "..";
 import PatitoInput from "../../../components/PatitoInput";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = NativeStackScreenProps<RootStackParamList, "HomeSearch">;
 
 const HomeSearch = ({ navigation }: Props) => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <PatitoInput />
+      <PatitoInput
+        icon={<Ionicons name="search-sharp" size={24} color="black" />}
+      />
       <Text onPress={() => navigation.navigate("Detail")}>Search Screen</Text>
     </View>
   );
