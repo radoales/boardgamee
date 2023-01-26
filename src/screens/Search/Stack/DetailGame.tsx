b70c38a7cbb7e0e9fecaf2d1ea6549d92b249af4
natsgt <nataliarodas7858@gmail.com>
2023-01-26 14:41:23 +0100

    create page for details

---
+++ src/screens/Search/Stack/DetailGame.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const DetailGame: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>DetailGame Screen</Text>
+    </View>
+  )
+}
+
+export default DetailGame
