ef9332dfa03ecc8440b9360bb3b2c6f80289bb82
natsgt <nataliarodas7858@gmail.com>
2023-01-26 14:39:26 +0100

    fix route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -3,7 +3,7 @@ export enum Route {
   PROFILE = 'Profile',
   SEARCH = 'Search',
   HOME_SEARCH = 'HomeSearch',
-  DEATIL = 'Detail',
+  DETAIL = 'Detail',
   LOG_IN = 'LogIn',
   SIGN_UP = 'SignUp',
   USER_PROFILE = 'UserProfile',
