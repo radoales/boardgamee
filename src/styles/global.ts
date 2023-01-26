4ff6b425698c3d031a47151e71d166f7fa690622
natsgt <nataliarodas7858@gmail.com>
2023-01-26 14:39:50 +0100

    create global style

---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+const globalStyles = StyleSheet.create({
+  container: {
+    paddingTop: '10%',
+    paddingHorizontal: '3%'
+  }
+})
+
+export default globalStyles
