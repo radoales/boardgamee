23d1f9109af191bf8a5715f7f1d24cb4a118055f
natsgt <nataliarodas7858@gmail.com>
2023-01-26 14:40:53 +0100

    change route and component

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import ResultList from './Stack/ResultList'
 import HomeSearch from './Stack/HomeSearch'
 import { Route } from '../../utils/routes'
+import DetailGame from './Stack/DetailGame'
 
 export type RootStackParamList = {
   HomeSearch: undefined
@@ -19,8 +20,8 @@ const SearchStack = () => {
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name={Route.DEATIL}
-        component={ResultList}
+        name={Route.DETAIL}
+        component={DetailGame}
         options={{ headerTitle: '', headerTransparent: true }}
       />
     </Stack.Navigator>
