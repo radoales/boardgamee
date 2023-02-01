2fa5941f8bc008d228271efef68d04e62cc9504c
radoslav.naydenov <radoslav.naydenov@wult.io>
2023-02-01 20:55:33 +0100

    add favorites hook

---
+++ src/hooks/favoriteGames.ts
@@ -0,0 +1,20 @@
+import { getDatabase, onValue, ref } from 'firebase/database'
+import { useEffect, useState } from 'react'
+import { app } from '../../App'
+
+export const UseGetFavoritesByUserId = (
+  id: string
+): { data?: { id: string }[] } => {
+  const [data, setData] = useState<{ id: string }[]>()
+
+  useEffect(() => {
+    const db = getDatabase(app)
+    const starCountRef = ref(db, `users/${id}/games`)
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val()
+      setData(data)
+    })
+  }, [])
+
+  return { data }
+}
