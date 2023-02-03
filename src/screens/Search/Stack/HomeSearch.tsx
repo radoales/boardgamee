083f7aa45a3dc319c613ea395162deee2b443600
natsgt <nataliarodas7858@gmail.com>
2023-02-03 09:46:22 +0100

    wait for fonts

--- src/screens/Search/Stack/HomeSearch.tsx
+++ src/screens/Search/Stack/HomeSearch.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from 'react'
 import {
   ActivityIndicator,
-  LogBox,
   ScrollView,
   StyleSheet,
   Text,
@@ -23,6 +22,22 @@ import { Game } from '../../../types/boardgame'
 
 type Props = NativeStackScreenProps<RootStackParamList, Route.HOME_SEARCH>
 
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flex: 1,
+    padding: '2%'
+  },
+  text: {
+    margin: 'auto',
+    alignSelf: 'center',
+    marginVertical: 'auto',
+    flex: 1,
+    fontFamily: 'Montserrat_400Regular'
+  }
+})
+
 const HomeSearch = ({ navigation }: Props) => {
   const [inputText, setInputText] = useState<string>('')
   const { results, isLoading } = useGetBoardgames(
@@ -34,53 +49,41 @@ const HomeSearch = ({ navigation }: Props) => {
     Montserrat_400Regular
   })
 
-  const styles = StyleSheet.create({
-    container: {
-      display: 'flex',
-      justifyContent: 'flex-start',
-      flex: 1,
-      padding: '2%'
-    },
-    text: {
-      margin: 'auto',
-      alignSelf: 'center',
-      marginVertical: 'auto',
-      flex: 1,
-      fontFamily: 'Montserrat_400Regular'
-    }
-  })
-
   const handlePress = (item: Game) => {
     setSelectedGame(item)
     navigation.navigate(Route.DETAIL)
   }
 
   return (
-    <View style={[styles.container, globalStyles.container]}>
-      <PatitoInput
-        icon={<Ionicons name='search-sharp' size={24} color='black' />}
-        onChange={(e) => setInputText(e.nativeEvent.text)}
-      />
+    <>
+      {fontsLoaded && (
+        <View style={[styles.container, globalStyles.container]}>
+          <PatitoInput
+            icon={<Ionicons name='search-sharp' size={24} color='black' />}
+            onChange={(e) => setInputText(e.nativeEvent.text)}
+          />
 
-      {!isLoading && !results && (
-        <Text style={styles.text}>Search using a boardgame name</Text>
+          {!isLoading && !results && (
+            <Text style={styles.text}>Search using a boardgame name</Text>
+          )}
+          {isLoading && <ActivityIndicator size='large' />}
+          {results && (
+            <ScrollView>
+              {results.games.map((item) => (
+                <TouchableHighlight
+                  key={item.id}
+                  activeOpacity={0.6}
+                  underlayColor={colors.gray[200]}
+                  onPress={() => handlePress(item)}
+                >
+                  <SearchResult data={item} />
+                </TouchableHighlight>
+              ))}
+            </ScrollView>
+          )}
+        </View>
       )}
-      {isLoading && <ActivityIndicator size='large' />}
-      {results && (
-        <ScrollView>
-          {results.games.map((item) => (
-            <TouchableHighlight
-              key={item.id}
-              activeOpacity={0.6}
-              underlayColor={colors.gray[200]}
-              onPress={() => handlePress(item)}
-            >
-              <SearchResult data={item} />
-            </TouchableHighlight>
-          ))}
-        </ScrollView>
-      )}
-    </View>
+    </>
   )
 }
 
