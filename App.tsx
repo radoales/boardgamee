78d831b91fdac1108ab660162786e81e0eedb8ad
natsgt <nataliarodas7858@gmail.com>
2023-02-06 21:02:53 +0100

    remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native'
 import TabMenuStackNaigator from './src/screens'
-import { initializeApp } from 'firebase/app'
 import { AuthUserProvider } from './src/auth/AuthUserprovider'
 import { LogBox } from 'react-native'
 import { GameProvider } from './src/hooks/gameContext'
-import { firebaseConfig } from './src/utils/apiConfig'
 
 LogBox.ignoreLogs([
   `AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storag`
