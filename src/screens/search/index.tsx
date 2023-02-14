ef494deda8e22eda79f3e7c51e644decfdcc878b
natsgt <nataliarodas7858@gmail.com>
2023-02-14 14:12:24 +0100

    change name to stack

--- src/screens/search/index.tsx
+++ src/screens/search/index.tsx
@@ -1,7 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import { StackScreenRoute } from '../../utils/routes'
 import GameDetails from '../../components/game/GameDetails'
-import SearchList from './stacks/SearchList'
+import SearchList from './stack/SearchList'
 
 export type SearchRootStackParamList = {
   [StackScreenRoute.SEARCH_LIST]: undefined
