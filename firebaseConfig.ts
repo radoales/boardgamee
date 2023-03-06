1c6c3066c491f7b7c2b0cac08a5760dc7c4199a3
radoslav.naydenov <radoslav.naydenov@wult.io>
2023-03-06 18:19:10 +0100

    fix refresh issue

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,4 +1,17 @@
 import { initializeApp } from 'firebase/app'
 import { firebaseConfig } from './src/utils/apiConfig'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import {
+  getReactNativePersistence,
+  initializeAuth
+} from 'firebase/auth/react-native'
+
 export const firebaseApp = initializeApp(firebaseConfig)
+
+const auth = initializeAuth(firebaseApp, {
+  persistence: getReactNativePersistence(AsyncStorage)
+})
+
+export { auth }
