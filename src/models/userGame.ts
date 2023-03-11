cd42cdda2499160af2f2bd42849c6c77d4363129
natsgt <nataliarodas7858@gmail.com>
2023-03-11 13:59:06 +0100

    create models for userGame

---
+++ src/models/userGame.ts
@@ -0,0 +1,11 @@
+export interface UserGame {
+  game_id: string
+  user_id: string
+  created_at: string
+  updated_at: string
+}
+
+export interface UserGameDto {
+  game_id: string
+  user_id: string
+}
