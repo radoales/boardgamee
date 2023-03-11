0d68eee471694040925d1fed0a31c49999f7f125
natsgt <nataliarodas7858@gmail.com>
2023-03-11 14:02:45 +0100

    add user to context when created

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,6 +2,10 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { StackScreenRoute } from '../../utils/routes'
 import HomeScreen from './stacks'
 import GameDetails from '../../components/game/GameDetails'
+import { useAuth } from '../../auth/AuthUserprovider'
+import { useContext, useEffect } from 'react'
+import { GameContext } from '../../hooks/gameContext'
+import { UseGetUserById } from '../../hooks/users'
 
 export type HomeRootStackParamList = {
   [StackScreenRoute.HOME]: undefined
@@ -10,6 +14,15 @@ export type HomeRootStackParamList = {
 const Stack = createStackNavigator<HomeRootStackParamList>()
 
 const HomeTabScreen = () => {
+  const { user } = useAuth()
+  const { data: userDetails } = UseGetUserById(user.id)
+  const { setUserId } = useContext(GameContext)
+
+  useEffect(() => {
+    if (userDetails) setUserId(userDetails.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userDetails])
+
   return (
     <Stack.Navigator>
       <Stack.Screen
